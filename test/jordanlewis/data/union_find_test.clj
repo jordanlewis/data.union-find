6de36c5ed5602001ab49b9b99b0079dd79036ecd
Jordan Lewis <jordan@knewton.com>
2012-12-28 17:46:55 -0500

    More tests

--- test/jordanlewis/data/union_find_test.clj
+++ test/jordanlewis/data/union_find_test.clj
@@ -5,7 +5,8 @@
 (deftest test-union-find
   (let [set (-> (union-find 1 2 3 4 5 6)
                 (connect 1 2)
-                (connect 3 4))]
+                (connect 3 4)
+                (connect 4 5))]
     (testing "Singleton sets are their own leaders."
       (is (= 6 (second (get-canonical set 6)))))
     (testing "Singleton sets unioned with themselves are still their own leaders."
@@ -14,16 +15,23 @@
       (let [[set a] (get-canonical set 1)
             [set b] (get-canonical set 2)
             [set c] (get-canonical set 3)
-            [set d] (get-canonical set 4)]
+            [set d] (get-canonical set 4)
+            [set e] (get-canonical set 5)]
         (is (= a b))
         (is (= c d))
+        (is (= c e))
         (is (not= b c))
         (let [set (connect set 2 3)
               [set a] (get-canonical set 1)
               [set c] (get-canonical set 3)]
           (is (= a c 1)))))
     (testing "Count counts the number of connected components."
-      (is (= (count set) 4)))
+      (is (= 3 (count set))))
+    (testing "Conj adds new singletons"
+      (let [set (conj set 7)]
+        (is (= 4 (count set)))
+        (is (= 7 (set 7)))
+        (is (= 6 ((connect set 6 7) 7)))))
 
     (testing "union-find is gettable"
       (is (= 1 (get set 2)))
