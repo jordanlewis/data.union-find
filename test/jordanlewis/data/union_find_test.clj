b25478574f24c137d4de82efebe4387059166efa
Jordan Lewis <jordan@knewton.com>
2012-12-28 10:53:13 -0500

    Tests for IFn and count

--- test/jordanlewis/data/union_find_test.clj
+++ test/jordanlewis/data/union_find_test.clj
@@ -8,8 +8,29 @@
   (testing "Singleton sets unioned with themselves are still their own leaders."
     (is (= 1 (second (get-canonical (connect (union-find 1) 1 1) 1)))))
   (testing "Connected singletons have the same leader."
-    (let [set (-> (union-find 1 2)
-                  (connect 1 2))
+    (let [set (-> (union-find 1 2 3 4 5 6)
+                  (connect 1 2)
+                  (connect 3 4))
           [set a] (get-canonical set 1)
-          [set b] (get-canonical set 2)]
-      (is (= a b)))))
+          [set b] (get-canonical set 2)
+          [set c] (get-canonical set 3)
+          [set d] (get-canonical set 4)]
+      (is (= a b))
+      (is (= c d))
+      (is (not= b c))
+      (let [set (connect set 2 3)
+            [set a] (get-canonical set 1)
+            [set c] (get-canonical set 3)]
+        (is (= a c 1)))))
+  (testing "Count counts the number of elts"
+    (is (= (count (union-find 1 2 3 4)) 4)))
+
+  (testing "union-find as function"
+    (let [set (-> (union-find 1 2)
+                  (connect 1 2))]
+      (is (= 1 (set 2)))
+      (is (= 1 (set 1)))
+      (is (= nil (set 3)))
+      (is (= :not-found (set 3 :not-found)))))
+
+  (testing "More map equalities"))
