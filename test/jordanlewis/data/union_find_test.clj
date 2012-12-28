7c34f74d6657060ac73e96c018115a0e13807154
Jordan Lewis <jordan@knewton.com>
2012-12-28 00:01:23 -0500

    Test that connecting 2 singletons works.

--- test/jordanlewis/data/union_find_test.clj
+++ test/jordanlewis/data/union_find_test.clj
@@ -6,5 +6,10 @@
   (testing "Singleton sets are their own leaders."
     (is (= 1 (second (get-canonical (union-find 1) 1)))))
   (testing "Singleton sets unioned with themselves are still their own leaders."
-    (is (= 1 (second (get-canonical (connect (union-find 1) 1 1) 1))))))
-
+    (is (= 1 (second (get-canonical (connect (union-find 1) 1 1) 1)))))
+  (testing "Connected singletons have the same leader."
+    (let [set (-> (union-find 1 2)
+                  (connect 1 2))
+          [set a] (get-canonical set 1)
+          [set b] (get-canonical set 2)]
+      (is (= a b)))))
