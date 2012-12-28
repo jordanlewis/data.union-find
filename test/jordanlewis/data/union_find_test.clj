9c40115fe4b64699561b8db4963d379423007986
Jordan Lewis <jordan@knewton.com>
2012-12-27 23:34:33 -0500

    Test that singleton sets unioned with themselves do nothing.

--- test/jordanlewis/data/union_find_test.clj
+++ test/jordanlewis/data/union_find_test.clj
@@ -2,7 +2,9 @@
   (:use clojure.test
         jordanlewis.data.union-find))
 
-(deftest singletons
+(deftest test-union-find
   (testing "Singleton sets are their own leaders."
-    (is (= 1 (second (get-canonical (union-find 1) 1))))))
+    (is (= 1 (second (get-canonical (union-find 1) 1)))))
+  (testing "Singleton sets unioned with themselves are still their own leaders."
+    (is (= 1 (second (get-canonical (connect (union-find 1) 1 1) 1))))))
 
