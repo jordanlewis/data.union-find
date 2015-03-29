f43867bc9c91299d8bb33ee9ecf4d875a56f6f76
Darrick Wiebe <dw@xnlogic.com>
2015-03-29 11:38:24 -0400

    Clean up benchmarks ns code duplication

--- dev/benchmarks.clj
+++ dev/benchmarks.clj
@@ -1,24 +1,7 @@
 (ns benchmarks
-  (:use jordanlewis.data.union-find)
+  (:use jordanlewis.data.union-find
+        dev-utils)
   (:require [criterium.core :refer [bench quick-bench]]))
-
-(defn make-stars [stars points]
-  (for [a (range stars)
-        b (range points)
-        :let [b (+ stars (* a points) b)]]
-    [a b (str b)]))
-
-(defn partition-graph [uf stars points -conj -union]
-  (reduce
-    (fn [uf [a b c]]
-      (-> uf
-          (-conj a)
-          (-conj b)
-          (-conj c)
-          (-union a b)
-          (-union b c)))
-    uf
-    (make-stars stars points)))
 
 (comment
 
